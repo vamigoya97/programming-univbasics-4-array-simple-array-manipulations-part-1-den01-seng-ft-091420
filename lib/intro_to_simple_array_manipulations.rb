faaf065ff80ec758a44751cb71f2964fa7d89a3f
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-03 17:26:37 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+  
+  
+  
+  
